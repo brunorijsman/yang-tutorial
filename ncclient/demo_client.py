76b6eadebd304e246650ca2d7dba519ea26396ab
Bruno Rijsman <brunorijsman@gmail.com>
2022-03-21 08:55:00 -0600

    commit at start of day

--- ncclient/demo_client.py
+++ ncclient/demo_client.py
@@ -4,8 +4,8 @@ with manager.connect(host="127.0.0.1",
                      port=830, 
                      username="parallels", 
                      password="Virtu33l?",
-                     hostkey_verify=False,
-                     device_params={'name':'alu'}) as m:
+                     hostkey_verify=False) as m:
+#                     device_params={'name':'alu'}) as m:
     c = m.get_config(source='running').data_xml
     with open("out.xml", 'w') as f:
         f.write(c)
